fd70e15ea5bd574ed1a9af31e7490309b3f82edd
NikolaYordanov94 <106193855+NikolaYordanov94@users.noreply.github.com>
2022-07-12 23:09:35 +0300

    Add files via upload

---
+++ DataTypeFinder_01.java
@@ -0,0 +1,20 @@
+package DataTypesAndVariablesMoreExercises;
+
+import java.util.Scanner;
+
+public class DataTypeFinder_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String dataType = scanner.nextLine();
+
+        while (!dataType.equals("END")){
+
+            double input = Double.parseDouble(dataType);
+
+            if(input / 10 == 0);
+        }
+
+
+    }
+}
