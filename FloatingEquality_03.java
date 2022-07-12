c2abe0aea85df62a2fe31c4f6b58f7777fce32ed
NikolaYordanov94 <106193855+NikolaYordanov94@users.noreply.github.com>
2022-07-12 23:10:15 +0300

    Add files via upload

---
+++ FloatingEquality_03.java
@@ -0,0 +1,20 @@
+package DataTypesAndVariablesMoreExercises;
+
+import java.util.Scanner;
+
+public class FloatingEquality_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double a = Double.parseDouble(scanner.nextLine());
+        double b = Double.parseDouble(scanner.nextLine());
+
+        double eps = 0.000001;
+
+        if (Math.abs(a - b) <= eps){
+            System.out.println("True");
+        }else{
+            System.out.println("False");
+        }
+    }
+}
